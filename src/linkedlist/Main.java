cdd4975ccfab5e863d02a52b97c3c1fb6dd5d8e2
Mang-Joo <wjwan0915@gmail.com>
2023-07-26 22:12:38 +0900

    :sparkles: feat: linked list

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -4,23 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyLinkedList<String> stringMyLinkedList = new MyLinkedList<>();
-        stringMyLinkedList.add("A");
-        stringMyLinkedList.add("B");
-        stringMyLinkedList.add("C");
-        stringMyLinkedList.add("D");
-        System.out.println("stringMyLinkedList.get(0) = " + stringMyLinkedList.get(0));
-        System.out.println("stringMyLinkedList.get(1) = " + stringMyLinkedList.get(1));
-        System.out.println("stringMyLinkedList.get(2) = " + stringMyLinkedList.get(2));
-        System.out.println("stringMyLinkedList.get(3) = " + stringMyLinkedList.get(3));
+        MyLinkedList<String> testLinkedList = new MyLinkedList<>();
+        testLinkedList.add("A");
+        testLinkedList.add("B");
+        testLinkedList.add("C");
+        testLinkedList.add("D");
+        System.out.println("stringMyLinkedList.get(0) = " + testLinkedList.get(0));
+        System.out.println("stringMyLinkedList.get(1) = " + testLinkedList.get(1));
+        System.out.println("stringMyLinkedList.get(2) = " + testLinkedList.get(2));
+        System.out.println("stringMyLinkedList.get(3) = " + testLinkedList.get(3));
 
-        stringMyLinkedList.remove(3);
-        stringMyLinkedList.remove(2);
-        stringMyLinkedList.remove(1);
-        stringMyLinkedList.remove(0);
+        testLinkedList.remove(3);
+        testLinkedList.remove(2);
+        testLinkedList.remove(1);
 
-        System.out.println("stringMyLinkedList = " + stringMyLinkedList.get(0));
+        System.out.println("stringMyLinkedList = " + testLinkedList.get(0));
 
+        testLinkedList.clear();
+        System.out.println("testLinkedList.get(0) = " + testLinkedList.get(0));
     }
 
 }
--- src/linkedlist/MyLinkedList.java
+++ src/linkedlist/MyLinkedList.java
@@ -6,11 +6,19 @@ public class MyLinkedList<E> {
     private Node first;
     private Node last;
 
+    @Override
+    public String toString() {
+        return "MyLinkedList{" +
+                "size=" + size +
+                ", first=" + first +
+                ", last=" + last +
+                '}';
+    }
 
     private class Node {
 
         private Node prev;
-        private E data;
+        private final E data;
         private Node next;
 
         public Node(E data) {
@@ -18,42 +26,67 @@ public class MyLinkedList<E> {
             this.data = data;
             this.next = null;
         }
+
+        public Node(Node prev, E data) {
+            this.prev = prev;
+            this.data = data;
+        }
     }
 
     public void add(E addData) {
         if (addData == null) {
             throw new IllegalArgumentException("Can not add null");
         }
-        if (first == null) {
-            first = new Node(addData);
-            size++;
-            return;
+        final Node lastNode = last;
+        final Node addNode = new Node(lastNode, addData);
+        last = addNode;
+        if (lastNode == null) {
+            first = addNode;
+        } else {
+            lastNode.next = addNode;
         }
-        if (last == null) {
-            last = new Node(addData);
-            first.next = last;
-            last.prev = first;
-            size++;
-            return;
-        }
-        Node add = new Node(addData);
-        searchByIndex(size).next = add;
-        last = add;
+
         size++;
     }
 
     public void remove(int index) {
-        if (index == 0) {
-            first = first.next;
+        checkIndex(index);
+        Node temp = searchByIndex(index);
+
+        Node prev = temp.prev;
+        Node next = temp.next;
+        if (prev == null) {
+            first = next;
         } else {
-            Node node = searchByIndex(index);
-            node.prev.next = node.next;
+            prev.next = next;
+            temp.prev = null;
         }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            temp.next = null;
+        }
+
         size--;
     }
 
+    public void clear() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
     public E get(int index) {
+        checkIndex(index);
         return searchByIndex(index).data;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
     }
 
     private Node searchByIndex(int index) {
@@ -68,7 +101,7 @@ public class MyLinkedList<E> {
             }
         } else {
             temp = last;
-            for (int i = size; i > index; i--) {
+            for (int i = size - 1; i > index; i--) {
                 temp = temp.prev;
             }
         }
